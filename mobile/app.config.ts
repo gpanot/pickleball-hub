d50b0cd3a7cb43dd389502f81499dd263443185e
gpanot <giompanot@gmail.com>
2026-05-23 16:47:40 +0700

    Your message describing the change

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -7,6 +7,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   extra: {
     apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "",
     googleClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID ?? "",
-    eas: { projectId: "" },
+    eas: { projectId: "06420ce0-0582-4866-afa5-5ec6e0b6c4ce" },
   },
 });
